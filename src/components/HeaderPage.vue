55ddbc8cc2408a9d31db84526c4a10ffe451abc6
marcomammi02 <marcomammi2002@gmail.com>
2022-11-24 22:56:33 +0100

    Main

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -23,15 +23,12 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap');
-
 .header {
   display: flex;
   justify-content: space-between;
   align-items: center;
   max-width: 1100px;
   margin: 0 auto;
-  font-family: 'Source Sans Pro', sans-serif;
 }
 
 .header img {
---
+++ src/components/MainPage.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="main">
+    <div class="content-area">
+      <h1>Content goes here</h1>
+    </div>
+    <div class="menu-area">
+      <ul>
+        <li>
+          <img src="@/img/buy-comics-digital-comics.png" alt="">
+          <p>DIGITAL COMICS</p>
+        </li>
+        <li>
+          <img src="@/img/buy-comics-merchandise.png" alt="">
+          <p>DC MERCHANDISE</p>
+        </li>
+        <li>
+          <img src="@/img/buy-comics-subscriptions.png" alt="">
+          <p>SUBSCRIPTION</p>
+        </li>
+        <li>
+          <img src="@/img/buy-comics-shop-locator.png" alt="">
+          <p>COMIC STORE LOCATOR</p>
+        </li>
+        <li>
+          <img src="@/img/buy-dc-power-visa.svg" alt="">
+          <p>DC POWER VISA</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+.main {
+  background-color: #0282f9;
+}
+
+.content-area {
+  background-color: #000000;
+  padding: 3rem 0;
+}
+
+.content-area h1 {
+  color: #ffffff;
+  font-size: 1.5rem;
+  margin-left: 15rem;
+}
+
+.menu-area {
+  max-width: 1100px;
+  padding: 3rem 0;
+  margin: 0 auto;
+}
+
+ul {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+li {
+  display: flex;
+  align-items: center;
+}
+
+img {
+  width: 40px;
+}
+
+li:nth-child(4) img{
+  width: 30px;
+}
+
+p {
+  color: #ffffff;
+  margin-left: 10px;
+}
+</style>
